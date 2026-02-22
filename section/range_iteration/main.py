6d7bf93c59b709675c40ee30202f414c579777a4
cheb3 <>
2026-02-22 20:49:27 +0000

    Range iteration

---
+++ section/range_iteration/main.py
@@ -0,0 +1,12 @@
+# List of countries
+countries = ['Wales', 'Denmark', 'Belgium', 'Japan', 'South Korea', 'South Africa', 'Indonesia', 'Singapore', 'Australia', 'India', 'Saudi Arabia', 'Mexico', 'Turkey', 'Greece', 'Netherlands', 'Finland', 'Monako', 'United Arab Emirates', 'Egypt', 'Morocco', 'Brazil', 'Argentina', 'Ireland', 'Portugal', 'Chile', 'Spain', 'Czech Republic', 'Sweden', 'Switzerland', 'Thailand', 'Luxemburg', 'New Zealand', 'France', 'Italy', 'Germany', 'China', 'Canada', 'Hungary', 'Scotland', 'Norway', 'Austria', 'Ukraine', 'Poland']
+
+# List of countries you agreed to visit
+your_travel_list = []
+# print(4%2)
+for i in range(len(countries)):
+    if i%2 ==0:
+        your_travel_list.append(countries[i])
+
+# Testing
+print(your_travel_list)
